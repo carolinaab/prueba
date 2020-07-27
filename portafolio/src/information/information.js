2a1699ee22ae3527f893df1ac08e1eb6d5845d9b
carolinaab <tau_karo@hotmail.com>
2020-07-26 23:29:55 -0500

    [ADD] flags proyect

--- portafolio/src/information/information.js
+++ portafolio/src/information/information.js
@@ -5,7 +5,7 @@ export const projectImages = [
         title: 'Github',
         link: 'Demo',
         width: '100%',
-        description:"Proyecto en equipo, que consistió en la creación de un inventario virtual. Participe en la creación de componentes y su funcionalidad",
+        description:"Proyecto en equipo, que consistió en la creación de un inventario virtual. Participe en la creación de componentes y su funcionalidad. Herramientas: HTML5,CSS3 y Javascript/React",
         page: "https://iselareyesperdomo94.github.io/BurguerQueenPandemonium/",
         github:"https://github.com/IselaReyesPerdomo94/BurguerQueenPandemonium"
     },
@@ -14,7 +14,7 @@ export const projectImages = [
         title: 'Github',
         link: 'Demo',
         width: '100%',
-        description:"Realizado en equipo, mi trabajo consistió en la funcionalidad de la página, creación de funciones e investigación para la extracción de datos",
+        description:"Realizado en equipo, mi trabajo consistió en la funcionalidad de la página, creación de funciones e investigación para la extracción de datos. Herramientas: HTML5,CSS3 y Javascript.",
         page: "https://gbarcenav.github.io/MEX-Data-Lovers-008/src/",
         github:"https://github.com/carolinaab/MEX-Data-Lovers-008"
     },
@@ -23,7 +23,7 @@ export const projectImages = [
         title: 'Github',
         link: 'Demo',
         width: '100%',
-        description:"La finalidad del proyecto es la practica de maquetación así como el aprendizaje de flexbox",
+        description:"La finalidad del proyecto es la practica de maquetación así como el aprendizaje de flexbox. Herramientas: HTML5 y CSS3",
         page: "https://carolinaab.github.io/MEX008-labcar/src/",
         github:"https://github.com/carolinaab/MEX008-labcar"
     },
@@ -32,8 +32,8 @@ export const projectImages = [
         title: 'Github',
         link: 'Demo',
         width: '100%',
-        description:"Web responsive que te permite consultar recetas de bebidas desde el curso: React - La Guía Completa: Hooks Context Redux MERN +15 Apps",
-        page:'https://laughing-bose-7e2e09.netlify.app/',
-        github:"https://github.com/carolinaab/Bebidas"
+        description:"Buscador de banderas, el cual va dirigido a ñiños de 4° y 5° de primaria para el conocimiento y aprendizaje de los principales países. Herramientas: HTML5,CSS3 y Javascript",
+        page:'https://carolinaab.github.io/banderas/src/',
+        github:"https://github.com/carolinaab/banderas"
     },
 ]
